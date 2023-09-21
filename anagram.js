df9653a0aa60b9d768343d6915aceb98fa4990a2
RipNasty <det625@gmail.com>
2023-09-21 15:08:10 -0400

    anagram finished

--- anagram.js
+++ anagram.js
@@ -6,8 +6,37 @@
 // letters exactly once.
 
 var isAnagram = function(s, t) {
-    const s = "anagram"
-    const t = "nagaram"
-
+    // Check if the lengths of the two strings are different
+    if (s.length !== t.length) {
+        return false; // They can't be anagrams if their lengths are different
+    }
     
+    // Create a character frequency map for string s
+    const charFrequencyS = {};
+    
+    // Iterate through string s and populate the frequency map
+    for (const char of s) {
+        if (charFrequencyS[char]) {
+            charFrequencyS[char]++;
+        } else {
+            charFrequencyS[char] = 1;
+        }
+    }
+    
+    // Iterate through string t and update the frequency map
+    // If we find a character in t that's not in the frequency map or its count becomes 0, return false
+    for (const char of t) {
+        if (!charFrequencyS[char]) {
+            return false;
+        }
+        charFrequencyS[char]--;
+    }
+    
+    // If we've successfully updated the frequency map for t, and all counts are 0, return true
+    return Object.values(charFrequencyS).every(count => count === 0);
 };
+
+// Example usage:
+const s = "anagram";
+const t = "nagaram";
+console.log(isAnagram(s, t)); // Output: true (t is an anagram of s)
